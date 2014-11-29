3526e4df934084955b6823d4d00cf7694b1eeb43
Derek Macias <derekmacias@live.com>
2014-11-29 11:42:55 -0700

    remove divs

--- classes/createvol.php
+++ classes/createvol.php
@@ -1,38 +1,35 @@
-<div class="wrap">
-	<div class="list">
-			<table class="tablesorter" style="margin-top:-21px"><thead><th colspan="2"><b>Create a new volume in pool <?=$pool?></b></th></thead> 
-				<form method="POST" action="?psubaction=volume-save&amp;pool=<?=$pool?>">
-						<tr><td>&nbsp;</td><td>&nbsp;</td></tr>
-						<tr align="left">
-							<td align="right">Volume name:&nbsp;</td>
-							<td align="left"><input type="text" autofocus name="disk[name]" title="name of volume" placeholder="Name of volume without extension"></td>
-						</tr>
-						<tr align="left">
-							<td align="right">Capacity:&nbsp;</td>
-							<td align="left"><input type="text" name="disk[capacity]" placeholder="e.g. 10M or 1G"></td>
-						</tr>
-						<tr align="left">
-							<td align="right">Allocation:&nbsp;</td>
-							<td align="left"><input type="text" name="disk[allocation]" placeholder="e.g. 10M or 1G"></td>
-						</tr>
-						<tr align="left">
-							<td align="right">Disk type:&nbsp;</td>
-							<td align="left"><select name="disk[driver]">
-														<option value="qcow2">qcow2</option>
-														<option value="raw">raw</option>
-														<option value="qcow">qcow</option>
-		    										</select>
-							</td>
-	    				</tr>
-						<tr align="right">
-							<td align="left"></td>
-							<td align="left">
-									<input type="submit" class="btn btn-sm btn-default" value="Save">
-									<button type="button" class="btn btn-sm btn-default" onclick="javascript:history.go(-1)" >Cancel</button>
-							</td>
-						</tr>
-						<input type="hidden" name="sent" value="1" />
-					</table>
-				</form>
-	</div>
-</div>
+<table class="tablesorter" style="margin-top:-21px"><thead><th colspan="2"><b>Create a new volume in pool <?=$pool?></b></th></thead> 
+	<form method="POST" action="?psubaction=volume-save&amp;pool=<?=$pool?>">
+		<tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+		<tr align="left">
+			<td align="right">Volume name:&nbsp;</td>
+			<td align="left"><input type="text" autofocus name="disk[name]" title="name of volume" placeholder="Name of volume without extension"></td>
+		</tr>
+		<tr align="left">
+			<td align="right">Capacity:&nbsp;</td>
+			<td align="left"><input type="text" name="disk[capacity]" placeholder="e.g. 10M or 1G"></td>
+		</tr>
+		<tr align="left">
+			<td align="right">Allocation:&nbsp;</td>
+			<td align="left"><input type="text" name="disk[allocation]" placeholder="e.g. 10M or 1G"></td>
+		</tr>
+		<tr align="left">
+			<td align="right">Disk type:&nbsp;</td>
+			<td align="left">
+				<select name="disk[driver]">
+					<option value="qcow2">qcow2</option>
+					<option value="raw">raw</option>
+					<option value="qcow">qcow</option>
+				</select>
+			</td>
+		</tr>
+		<tr align="right">
+			<td align="left"></td>
+			<td align="left">
+				<input type="submit" value="Save">
+				<button type="button" onclick="javascript:history.go(-1)" >Cancel</button>
+			</td>
+		</tr>
+			<input type="hidden" name="sent" value="1" />
+	</table>
+</form>
--- classes/editxml.php
+++ classes/editxml.php
@@ -1,38 +1,34 @@
-<div class="wrap">
-	<div class="list">
-	<?php
-	$domName = $lv->domain_get_name_by_uuid($uuid);
-   $xml = $lv->domain_get_xml($domName);
-	if ($_GET['readonly']) {
-		$readonly = 'readonly';
-		$method = "View";
-		$type = "hidden";
-		$return = "Back";
-	}
-	else {	
-		$method = "Edit";
-		$type = "submit";
-		$return = "Cancel";
-	}
-	?>
-		<div>
-			<form method="POST" id="editXML" action="?subaction=domain-create&uuid=<?=$uuid;?>" >
-				<table class="tablesorter" style="margin-top:-33px"><thead><th colspan="2"><b><?=$method;?> Domain <?=$domName;?> XML Description</b></th></thead>
-					<tr>
-						<td>
-							<textarea autofocus <?=$readonly?> name="xmldesc" rows="15" cols="50"><?=$xml;?></textarea>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<div>
-								<input type="<?=$type;?>" class="btn btn-sm btn-default" value="Save">
-								<button type="button" class="btn btn-sm btn-default" onclick="javascript:done()" ><?=$return?></button>
-							</div>
-						</td>
-					</tr>
-				</table>
-			</form>
-		</div>
-	</div>
+<?php
+$domName = $lv->domain_get_name_by_uuid($uuid);
+$xml = $lv->domain_get_xml($domName);
+if ($_GET['readonly']) {
+	$readonly = 'readonly';
+	$method = "View";
+	$type = "hidden";
+	$return = "Back";
+}
+else {	
+	$method = "Edit";
+	$type = "submit";
+	$return = "Cancel";
+}
+?>
+<div>
+	<form method="POST" id="editXML" action="?subaction=domain-create&uuid=<?=$uuid;?>" >
+		<table class="tablesorter" style="margin-top:-33px"><thead><th colspan="2"><b><?=$method;?> Domain <?=$domName;?> XML Description</b></th></thead>
+			<tr>
+				<td>
+					<textarea autofocus <?=$readonly?> name="xmldesc" rows="15" cols="50"><?=$xml;?></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div>
+						<input type="<?=$type;?>" value="Save">
+						<button type="button" onclick="javascript:done()" ><?=$return?></button>
+					</div>
+				</td>
+			</tr>
+		</table>
+	</form>
 </div>
