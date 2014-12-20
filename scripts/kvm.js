f444d471b021d3d350539d2e2400bd257567c7ce
Derek Macias <derekmacias@live.com>
2014-12-20 16:24:32 -0700

    added drivers filetree

--- scripts/kvm.js
+++ scripts/kvm.js
@@ -73,6 +73,12 @@ function showMedia(){
     $('#media_tree').focus();
 }
 
+function showDrivers(){
+    $('#drivers_file').on('click');
+    $('#drivers_tree').show();
+    $('#drivers_tree').focus();
+}
+
 function showDisk(){
     $('#disk_file').on('click');
     $('#disk_tree').show();
@@ -90,10 +96,24 @@ $(function(){
    	{root:'/mnt/',filter:['iso','ISO'],script:'/plugins/dynamix.kvm.manager/classes/FileTree.php',multiFolder:false},
    	function(file) {$('#cdrom_file').val(file);$('#cdrom_tree').hide();$('#cdrom_form').submit();},
    	function(directory) {$('#cdrom_dir').val(directory);});
-	$('#media_tree').fileTree({root:'/mnt/',filter:['iso','ISO'],script:'/plugins/dynamix.kvm.manager/classes/FileTree.php',multiFolder:false}, function(file) {$('#media_file').val(file);$('#media_tree').hide();}, function(directory) {$('#media_dir').val(directory);});
-	$('#disk_tree').fileTree({root:'/mnt/',filter:['qcow2','qcow','img'],script:'/plugins/dynamix.kvm.manager/classes/FileTree.php',multiFolder:false}, function(file) {$('#disk_file').val(file);$('#disk_tree').hide();}, function(directory) {$('#disk_file').val(directory);});
-	$('#new_disk_tree').fileTree({root:'/mnt/',filter:['.'],script:'/plugins/dynamix.kvm.manager/classes/FileTree.php',multiFolder:false}, function(file) {$('#new_disk_file').val(file);$('#new_disk_tree').hide();}, function(directory) {$('#new_disk_file').val(directory);});
+	$('#media_tree').fileTree(
+		{root:'/mnt/',filter:['iso','ISO'],script:'/plugins/dynamix.kvm.manager/classes/FileTree.php',multiFolder:false},
+		function(file) {$('#media_file').val(file);$('#media_tree').hide();},
+		function(directory) {$('#media_dir').val(directory);});
+	$('#drivers_tree').fileTree(
+		{root:'/mnt/',filter:['iso','ISO'],script:'/plugins/dynamix.kvm.manager/classes/FileTree.php',multiFolder:false},
+		function(file) {$('#drivers_file').val(file);$('#drivers_tree').hide();},
+		function(directory) {$('#drivers_dir').val(directory);});
+	$('#disk_tree').fileTree(
+		{root:'/mnt/',filter:['qcow2','qcow','img'],script:'/plugins/dynamix.kvm.manager/classes/FileTree.php',multiFolder:false},
+		function(file) {$('#disk_file').val(file);$('#disk_tree').hide();},
+		function(directory) {$('#disk_file').val(directory);});
+	$('#new_disk_tree').fileTree(
+		{root:'/mnt/',filter:['.'],script:'/plugins/dynamix.kvm.manager/classes/FileTree.php',multiFolder:false},
+		function(file) {$('#new_disk_file').val(file);$('#new_disk_tree').hide();},
+		function(directory) {$('#new_disk_file').val(directory);});
 	$('#media_file').click(showMedia);
+	$('#drivers_file').click(showDrivers);
 	$('#disk_file').click(showDisk);
 	$('#new_disk_file').click(showNewDisk);
 	$('#cdrom_file').click(showCDRom);
@@ -105,6 +125,9 @@ $(function(){
     	};
     	if (!$(event.target).closest('#media_file').length) {
       	  $('#media_tree').hide();
+    	};
+    	if (!$(event.target).closest('#drivers_file').length) {
+      	  $('#drivers_tree').hide();
     	};
     	if (!$(event.target).closest('#disk_file').length) {
       	  $('#disk_tree').hide();
