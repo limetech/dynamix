4b92c384bd352709e30563b21656bca9bd1e7c72
gfjardim <gfjardim@gmail.com>
2015-03-04 19:40:35 -0300

    Do some cleanup on dockerupdate.php

--- plugins/dynamix.docker.manager/dockerupdate.php
+++ plugins/dynamix.docker.manager/dockerupdate.php
@@ -1,27 +1,15 @@
 #!/usr/bin/php
 <?
 require_once("/usr/local/emhttp/plugins/dynamix.docker.manager/dockerClient.php");
-$DockerUpdate = new DockerUpdate();
 $DockerTemplates = new DockerTemplates();
 
-$verbose = ($argv[1] == "-v") ? TRUE : FALSE;
+if ($argv[1] == "-v") $DockerTemplates->verbose = TRUE;
 
 echo " Updating templates... ";
-$out = $DockerTemplates->downloadTemplates();
-if ($verbose) foreach ($out as $value) echo "\n$value";
+$DockerTemplates->downloadTemplates();
 
 echo " Updating info... ";
-$out = $DockerTemplates->getAllInfo(TRUE);
-if ($verbose) {
-  echo "\n\nUpdating info... ";
-  foreach ($out as $key => $value){
-    echo "\n$key" ;
-    foreach ($value as $k => $v){
-      printf("\n   %-10s: %s", $k, $v);
-    }
-    echo "\n";
-  }
-}
+$DockerTemplates->getAllInfo(TRUE);
 
 echo " Done. ";
 ?>
