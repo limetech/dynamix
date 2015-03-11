cacabe3671087ea0a4cefa2391a54a651c412f2d
Eric Schultz <eric@startuperic.com>
2015-03-10 19:03:36 -0500

    added javascript functions to help with the TreePicker, slide effects and template loading

--- scripts/dynamix.kvm.manager.js
+++ scripts/dynamix.kvm.manager.js
@@ -8,7 +8,7 @@ $(function(){
 });
 
 function clearHistory(){
-	window.history.pushState('KVM', 'Title', '/KVM');
+	window.history.pushState('VMs', 'Title', '/VMs');
 }
 
 function vncOpen() {
@@ -17,26 +17,88 @@ function vncOpen() {
   });
 }
 
-function toggleRows(what, val, what2) {
-	if (val == 1){
-		$('.'+what).show();
-		if (what2 != null)
-			$('.'+what2).hide();
-	} else {
-		$('.'+what).hide();
-		if (what2 != null)
-			$('.'+what2).show();
-	}		
+function loadTemplateForm(val) {
+  var el = $('#form_content');
+
+  el.fadeOut('fast', function(){
+    el.html('<span style="padding-left: 20px"><img src="/webGui/images/spinner.gif"> Loading...</span>').fadeIn('fast');
+  });
+
+  $.get('/plugins/dynamix.kvm.manager/templates/' + val + '.form.php', function(data) {
+    if (el.is(':animated')) {
+      el.stop().html(data).animate({opacity:'100'});
+    } else {
+      el.hide().html(data).fadeIn('fast');
+    }
+  });
 }
 
-function toggle_id(itemID){ 
-   if ((document.getElementById(itemID).style.display == 'none')) { 
+function slideUpRows($tr) {
+  if ($tr.is(':visible')) {
+    if ($tr.find(':animated').length === 0) {
+      $tr.children('td').each(function(){
+        $(this)
+          .stop(true, true)
+          .data("paddingstate", $(this).css(["paddingTop", "paddingBottom"]))
+          .animate({ paddingTop: 0, paddingBottom: 0 }, { duration: 'fast' })
+          .wrapInner('<div />')
+          .children()
+          .slideUp("fast", function() {
+            $(this).contents().unwrap();
+            $tr.hide();
+          });
+      });
+    }
+  }
+
+  return $tr;
+}
+
+function slideDownRows($tr) {
+  if (!$tr.is(':visible')) {
+    if ($tr.find(':animated').length === 0) {
+      $tr.children('td').each(function(){
+        $(this)
+          .stop(true, true)
+          .wrapInner('<div style="display: none"></div>')
+          .animate($(this).data("paddingstate"), { duration: 'fast', start: function() { $(this).closest('tr').show(); } })
+          .children('div')
+          .slideDown("fast", function() {
+            $(this).contents().unwrap();
+          });
+      });
+    }
+  }
+
+  return $tr;
+}
+
+function toggleRows(what, val, what2) {
+	if (val == 1) {
+		//$('.'+what).show();
+    slideDownRows($('.'+what));
+		if (what2 != null) {
+			//$('.'+what2).hide();
+      slideUpRows($('.'+what2));
+    }
+	} else {
+		//$('.'+what).hide();
+    slideUpRows($('.'+what));
+		if (what2 != null) {
+			//$('.'+what2).show();
+      slideDownRows($('.'+what2));
+    }
+	}
+}
+
+function toggle_id(itemID){
+   if ((document.getElementById(itemID).style.display == 'none')) {
       document.getElementById(itemID).style.display = 'table-row';
       event.preventDefault();
-   } else { 
-      document.getElementById(itemID).style.display = 'none'; 
+   } else {
+      document.getElementById(itemID).style.display = 'none';
       event.preventDefault();
-   }    
+   }
 }
 
 function showInput(){
@@ -50,6 +112,50 @@ function hideInput(){
     $(this).hide();
     $(this).siblings('span').show();
     $(this).siblings('span').click(showInput);
+}
+
+
+function universalTreePicker() {
+  var input = $(this);
+  var config = input.data();
+  var picker = input.next(".fileTree");
+
+  if (picker.length === 0) {
+    $(document).mouseup(function (e) {
+      var container = $(".fileTree");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        container.slideUp('fast');
+      }
+    });
+
+    picker = $('<div>', {'class': 'textarea fileTree'});
+
+    input.after(picker);
+  }
+
+  if (picker.is(':visible')) {
+    picker.slideUp('fast');
+  } else {
+    if (picker.html() == "") {
+      picker.fileTree({
+        root: config.pickroot,
+        filter: (config.pickfilter || '').split(",")
+      },
+      function(file) {
+        input.val(file).change();
+        if (config.pickcloseonfile) {
+          picker.slideUp('fast');
+        }
+      },
+      function(folder) {
+        if (config.pickfolders) {
+          input.val(folder).change();
+        }
+      });
+    }
+
+    picker.slideDown('fast');
+  }
 }
 
 function CDRomTree(cdrom_treeID, cdrom_fileID, cdrom_formID){
