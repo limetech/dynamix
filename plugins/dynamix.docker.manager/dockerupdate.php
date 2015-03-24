64f4a38fde7a78295b3fd9f491e7d1042fbca5b7
gfjardim <gfjardim@gmail.com>
2015-03-24 20:34:29 -0300

    Add Docker to Notifications.

--- plugins/dynamix.docker.manager/dockerupdate.php
+++ plugins/dynamix.docker.manager/dockerupdate.php
@@ -3,13 +3,42 @@
 require_once("/usr/local/emhttp/plugins/dynamix.docker.manager/dockerClient.php");
 $DockerTemplates = new DockerTemplates();
 
-if ($argv[1] == "-v") $DockerTemplates->verbose = TRUE;
+switch ($argv[1]) {
+  case '-v':
+    $DockerTemplates->verbose = TRUE;
+    break;
+  case 'check':
+    $check = TRUE;
+    break;
+}
 
-echo " Updating templates... ";
+echo isset($check) ? "": " Updating templates... ";
 $DockerTemplates->downloadTemplates();
 
-echo " Updating info... ";
+echo isset($check) ? "": " Updating info... ";
 $DockerTemplates->getAllInfo(TRUE);
 
-echo " Done. ";
+echo isset($check) ? "": " Done. ";
+
+if (isset($check)) { 
+  require_once("/usr/local/emhttp/webGui/include/Wrappers.php");
+  $DockerClient = new DockerClient();
+  $notify = "/usr/local/sbin/notify";
+  $unraid = parse_plugin_cfg("dynamix",true);
+  $server = strtoupper($var['NAME']);
+  $output = $unraid['notify']['docker_notify'];
+  
+  $all_containers = $DockerClient->getDockerContainers();
+  $info = $DockerTemplates->getAllInfo();
+  foreach($all_containers as $ct){
+    $name           = $ct["Name"];
+    $hasUpdate   = ($info[$name]['updated'] == "false") ? TRUE : FALSE;
+    if ($hasUpdate) {
+      exec("$notify -e 'Docker Manager' -s 'Notice [$server] - $name: new version available' -d 'A new version of $name is available.' -i 'normal $output' -x");
+    }
+  }
+}
+
+exit(0);
+
 ?>
