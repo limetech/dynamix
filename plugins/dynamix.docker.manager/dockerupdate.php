9df7d5bb3ce9becca4edd1ac960b41ba49e34a62
Bergware <ronald.vd.berg62@gmail.com>
2015-03-28 15:10:27 +0100

    Correction in Docker update check
    Under certain circumstances new Docker versions were not properly
    detected.

--- plugins/dynamix.docker.manager/dockerupdate.php
+++ plugins/dynamix.docker.manager/dockerupdate.php
@@ -1,46 +1,40 @@
 #!/usr/bin/php
-<?
+<?PHP
 require_once("/usr/local/emhttp/plugins/dynamix.docker.manager/dockerClient.php");
-$DockerTemplates = new DockerTemplates();
+
+$docker = new DockerTemplates();
 
 foreach ($argv as $arg) {
   switch ($arg) {
-    case '-v':
-      $DockerTemplates->verbose = TRUE;
-      break;
-    case 'check':
-      $check = TRUE;
-      break;
-  }
+  case '-v'   : $docker->verbose = true; break;
+  case 'check': $check = true; break;}
 }
+
 if (!isset($check)) {
   echo " Updating templates... ";
-  $DockerTemplates->downloadTemplates();
-}
-
-if (!isset($check)) echo " Updating info... ";
-$DockerTemplates->getAllInfo(TRUE);
-if (!isset($check)) echo " Done. ";
-
-if (isset($check)) { 
+  $docker->downloadTemplates();
+  echo " Updating info... ";
+  $docker->getAllInfo(true);
+  echo " Done.";
+} else {
   require_once("/usr/local/emhttp/webGui/include/Wrappers.php");
-  $DockerClient = new DockerClient();
+  $client = new DockerClient();
+  $update = new DockerUpdate();
   $notify = "/usr/local/sbin/notify";
   $unraid = parse_plugin_cfg("dynamix",true);
   $server = strtoupper($var['NAME']);
   $output = $unraid['notify']['docker_notify'];
-  
-  $all_containers = $DockerClient->getDockerContainers();
-  $info = $DockerTemplates->getAllInfo();
-  foreach($all_containers as $ct){
-    $name           = $ct["Name"];
-    $hasUpdate   = ($info[$name]['updated'] == "false") ? TRUE : FALSE;
-    if ($hasUpdate) {
-      exec("$notify -e 'Docker Manager - $name update available' -s 'Notice [$server] - $name: new version available' -d 'A new version of $name is available.' -i 'normal $output' -x");
+
+  $list = $client->getDockerContainers();
+  $info = $docker->getAllInfo();
+  foreach ($list as $ct) {
+    $name = $ct['Name'];
+    $image = $ct['Image'];
+    if ($info[$name]['updated'] != "false") {
+      $new = $update->getRemoteVersion($docker->getTemplateValue($image, "Registry"), $image);
+      exec("$notify -e 'Docker - $name [$new]' -s 'Notice [$server] - Docker update $new' -d 'A new version of $name is available' -i 'normal $output' -x");
     }
   }
 }
-
 exit(0);
-
 ?>
