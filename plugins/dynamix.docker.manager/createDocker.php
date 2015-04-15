3de35ffaf17710dfdc9fe8ec3b77cb68f4e17a03
gfjardim <gfjardim@gmail.com>
2015-04-15 10:08:39 -0300

    Remove old container if renamed in edit

--- plugins/dynamix.docker.manager/createDocker.php
+++ plugins/dynamix.docker.manager/createDocker.php
@@ -251,6 +251,13 @@ if ($_POST){
     include($dockerManPaths['plugin'] . "/exec.php");
   }
 
+  // Remove old container if renamed
+  $existing = isset($_POST['existingContainer']) ? $_POST['existingContainer'] : FALSE;
+  if ($existing && ContainerExist($existing)){
+    $_GET['cmd'] = "/usr/bin/docker rm -f $existing";
+    include($dockerManPaths['plugin'] . "/exec.php"); 
+  }
+
   // Injecting the command in $_GET variable and executing.
   $_GET['cmd'] = $cmd;
   include($dockerManPaths['plugin'] . "/exec.php");
@@ -579,7 +586,9 @@ $showAdditionalInfo = true;
 <div id="canvas" style="z-index:1;">
   <form method="post" id="createContainer">
     <table class="Preferences">
-      <? if($xmlType != "edit") {?>
+      <? if($xmlType == "edit"):
+        if (ContainerExist($templateName)): echo "<input type='hidden' name='existingContainer' value='${templateName}'>\n"; endif;
+      else:?>
       <tr>
         <td style="width: 150px;">Template:</td>
         <td >
@@ -618,7 +627,7 @@ $showAdditionalInfo = true;
           }?>
         </td>
       </tr>
-      <?}?>
+      <?endif;?>
       <?if(!empty($templateDescBox)){?>
       <tr>
         <td style="vertical-align: top;">Description:</td>
