f45d1431401ae3eb17344f3addaff6b7fa5a5cda
gfjardim <gfjardim@gmail.com>
2015-04-19 15:34:45 -0300

    Add size limit and error detection

--- plugins/dynamix/include/UploadLog.php
+++ plugins/dynamix/include/UploadLog.php
@@ -1,5 +1,6 @@
 <?
-$url = "http://162.243.138.66";
+$url = "http://unraid.mylog.ml";
+$max_size = 2097152; # in bytes
 
 if (! isset($var)){
   if (! is_file("/usr/local/emhttp/state/var.ini")) shell_exec("wget -qO /dev/null localhost:$(ss -napt|grep emhttp|grep -Po ':\K\d+')");
@@ -17,13 +18,16 @@ function sendPaste($text, $title) {
   file_put_contents($tmpfile, json_encode($data));
   $out = shell_exec("curl -s -k -L -X POST -H 'Content-Type: application/json' -d '@$tmpfile' ${url}/api/json/create");
   unlink($tmpfile);
+  $notify = "/usr/local/sbin/notify";
+  $server = strtoupper($var['NAME']);
   $out = json_decode($out, TRUE);
-  if (isset($out['result'])){
+  if (isset($out['result']['error'])){
+    echo shell_exec("$notify -e '$title upload failed' -s 'Alert [$server] - $title upload failed.' -d 'Upload of $title has failed: ".$out['result']['error']."' -i 'alert 1'");
+    echo '{"result":"failed"}';
+  } else {
     $resp = "${url}/".$out['result']['id']."/".$out['result']['hash'];
-    $notify = "/usr/local/sbin/notify";
-    $server = strtoupper($var['NAME']);
-    exec("$notify -e '$title uploaded - [".$out['result']['id']."]' -s 'Notice [$server] - $title uploaded.' -d 'A new copy of $title has been uploaded: $resp' -i 'normal 1' -x");
-    echo $resp;
+    exec("$notify -e '$title uploaded - [".$out['result']['id']."]' -s 'Notice [$server] - $title uploaded.' -d 'A new copy of $title has been uploaded: $resp' -i 'normal 1'");
+    echo '{"result":"'.$resp.'"}';
   }
 }
 
@@ -33,13 +37,13 @@ if ($_POST['pastebin']){
   if (!$text) exit(0);
   switch ($_POST['type']) {
     case 'file':
-      sendPaste(file_get_contents($text),$title);
+      sendPaste(shell_exec("/usr/bin/cat '$text' 2>&1 | tail -c $max_size -"), $title);
       break;
     case 'command':
-      sendPaste(shell_exec($text." 2>&1"), $title);
+      sendPaste(shell_exec($text." 2>&1 | tail -c $max_size -"), $title);
       break;
     case 'text':
-      sendPaste($text, $title);
+      sendPaste(shell_exec("/usr/bin/echo '$text' 2>&1 | tail -c $max_size -"), $title);
       break;
   }
 }
