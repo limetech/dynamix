84b384af7a1a7f6274ae8c0fc1f609c29ad356c1
gfjardim <gfjardim@gmail.com>
2015-04-20 14:52:44 -0300

    Revert ff36cba..921542c
    This rolls back to commit ff36cbae547ab496fa68f5536b4a1c68330bb430.

--- plugins/dynamix/include/DashUpdate.php
+++ plugins/dynamix/include/DashUpdate.php
@@ -108,7 +108,7 @@ case 'cpu':
   if (file_exists("/proc/xen")) {
     exec("xenpm get-cpufreq-states|awk '/^current frequency/ {print \$4\" \"\$5}'",$speeds);
   } else {
-    exec("awk '/^cpu MHz/ {printf \"%4.0f MHz\\n\", $4}' /proc/cpuinfo|head -$(awk '/^cpu cores/ {print $4;exit;}' /proc/cpuinfo)",$speeds);
+    exec("awk '/^cpu MHz/ {printf\"%4.0f MHz\\n\", $4}' /proc/cpuinfo",$speeds);
   }
   echo implode('#',$speeds);
 break;
