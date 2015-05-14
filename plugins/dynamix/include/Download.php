3ab842cea97fc3b809cb2f0e0c1947efc9caab53
Ronald van den Berg <ronald.vd.berg62@gmail.com>
2015-05-14 22:20:03 +0200

    New diagnostics utility

--- plugins/dynamix/include/Download.php
+++ plugins/dynamix/include/Download.php
@@ -12,11 +12,10 @@
 ?>
 
 <?
-$file = $_POST['file'];
-
 switch ($_POST['cmd']) {
 case 'save':
   $raw  = empty($_POST['raw']) ? false : $_POST['raw'];
+  $file = $_POST['file'];
   $root = $_POST['root'];
   $name = basename($file);
   if ($raw == false) {
@@ -35,6 +34,24 @@ case 'save':
   echo "/$name";
   break;
 case 'delete':
-  @unlink($file);
-  break;}
+  @unlink($_POST['file']);
+  break;
+case 'diag':
+  $zip = $_POST['file'];
+  exec("mkdir -p /diagnostics/array /diagnostics/config /diagnostics/log /diagnostics/shares /diagnostics/smart");
+  file_put_contents("/diagnostics/array/array.txt", str_replace("\n","\r\n",print_r(parse_ini_file('/var/local/emhttp/disks.ini',true),true)));
+  foreach (glob("/boot/config/*.cfg") as $file) exec("todos <$file >/diagnostics/config/".basename($file,'.cfg').".txt");
+  exec("cp /boot/config/super.dat /diagnostics/config");
+  exec("todos </boot/config/go >/diagnostics/config/go.txt");
+  foreach (glob("/var/log/syslog*") as $file) exec("todos <$file >/diagnostics/log/".basename($file).".txt");
+  foreach (glob("/boot/config/shares/*.cfg") as $file) exec("todos <$file >/diagnostics/shares/".basename($file,'.cfg').".txt");
+  exec("ls -l /dev/disk/by-id/[au]*|awk '$0!~/-part/{split($11,a,\"/\");print a[3],substr($9,21)}'|sort", $devices);
+  foreach ($devices as $device) {
+    $disk = explode(' ',$device);
+    exec("smartctl -a /dev/${disk[0]} >/diagnostics/smart/${disk[1]}.txt");
+  }
+  exec("zip -qmr $zip /diagnostics");
+  echo "/".basename($zip);
+  break;
+}
 ?>
