d4e47acb0ab9cfccd24cc367a2c0f6913e8045b1
limetech <tomm@lime-technology.com>
2015-08-20 09:28:50 -0700

    Changes to support escapeshellarg().

--- update.php
+++ update.php
@@ -31,31 +31,33 @@
  */
 function write_log($string) {
   if (empty($string)) return;
-  syslog(LOG_INFO, $string);
   $string = str_replace("\n", "<br>", $string);
   $string = str_replace('"', "\\\"", trim($string));
   echo "<script>addLog(\"{$string}\");</script>";
   @flush();
 }
+// not provided by emhttp
+$_REQUEST = array_merge($_GET, $_POST);
+
 // unRAID update control
 readfile('update.htm');
 
-$file = isset($_POST['#file']) ? $_POST['#file'] : false;
-$command = isset($_POST['#command']) ? $_POST['#command'] : false;
+$file = isset($_REQUEST['#file']) ? $_REQUEST['#file'] : false;
+$command = isset($_REQUEST['#command']) ? $_REQUEST['#command'] : false;
 $docroot = $_SERVER['DOCUMENT_ROOT'];
 
 if ($file) {
 // prepend with boot (flash) if path is relative
   if ($file[0]!='/') $file = "/boot/config/plugins/$file";
-  $section = isset($_POST['#section']) ? $_POST['#section'] : false;
-  $cleanup = isset($_POST['#cleanup']);
-  $default = isset($_POST['#default']) ? @parse_ini_file("$docroot/plugins/".basename(dirname($file))."/default.cfg", $section) : array();
+  $section = isset($_REQUEST['#section']) ? $_REQUEST['#section'] : false;
+  $cleanup = isset($_REQUEST['#cleanup']);
+  $default = isset($_REQUEST['#default']) ? @parse_ini_file("$docroot/plugins/".basename(dirname($file))."/default.cfg", $section) : array();
 
   $keys = @parse_ini_file($file, $section);
 // the 'save' switch can be reset by the include file to disallow settings saving
   $save = true;
-  if (isset($_POST['#include'])) {
-    $include = realpath($docroot.'/'.$_POST['#include']);
+  if (isset($_REQUEST['#include'])) {
+    $include = realpath($docroot.'/'.$_REQUEST['#include']);
     if (strpos($include, $docroot) === 0) include $include; else {
       syslog(LOG_INFO, "Include file not allowed: $include. Settings not saved!");
       $save = false;
@@ -64,13 +66,13 @@ if ($file) {
   if ($save) {
     $text = "";
     if ($section) {
-      foreach ($_POST as $key => $value) if ($key[0]!='#') $keys[$section][$key] = isset($default[$section][$key]) ? $default[$section][$key] : $value;
+      foreach ($_REQUEST as $key => $value) if ($key[0]!='#') $keys[$section][$key] = isset($default[$section][$key]) ? $default[$section][$key] : $value;
       foreach ($keys as $section => $block) {
         $text .= "[$section]\n";
         foreach ($block as $key => $value) if (strlen($value) || !$cleanup) $text .= "$key=\"$value\"\n";
       }
     } else {
-      foreach ($_POST as $key => $value) if ($key[0]!='#') $keys[$key] = isset($default[$key]) ? $default[$key] : $value;
+      foreach ($_REQUEST as $key => $value) if ($key[0]!='#') $keys[$key] = isset($default[$key]) ? $default[$key] : $value;
       foreach ($keys as $key => $value) if (strlen($value) || !$cleanup) $text .= "$key=\"$value\"\n";
     }
     @mkdir(dirname($file));
@@ -78,20 +80,23 @@ if ($file) {
   }
 }
 if ($command) {
-  if (isset($_POST['#env'])) {
-    foreach ($_POST['#env'] as $env) putenv($env);
+  if (isset($_REQUEST['#env'])) {
+    foreach ($_REQUEST['#env'] as $env) putenv($env);
   }
   if (strpos($command, $docroot) === 0)
     syslog(LOG_INFO, "Deprecated absolute #command path: $command");
   else if ($command[0] != '/')
     syslog(LOG_INFO, "Deprecated relative #command path: $command");
+  else if (strpos($command, " "))
+    syslog(LOG_INFO, "Invalid #command: $command");
   else
-    $command = $docroot.$command;
-  if (isset($_POST['#arg'])) {
-    $args = $_POST['#arg'];
+    $command = escapeshellcmd($docroot.$command);
+  if (isset($_REQUEST['#arg'])) {
+    $args = $_REQUEST['#arg'];
     ksort($args);
-    $command .= " ".implode(" ", $args);
+    $command .= " ".implode(" ", array_map("escapeshellarg", $args));
   }
+  syslog(LOG_INFO, $command);
   write_log($command);
   $proc = popen($command, 'r');
   while (!feof($proc)) {
